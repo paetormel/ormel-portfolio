ae9ac68a958e348bc9bc294ab4f27843dadb5899
Ormel Paet <ormelpaet653@gmail.com>
2026-03-12 12:10:12 +0800

    feat: Update Navbar component with improved motion effects and social links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaFacebook, FaGithub } from "react-icons/fa";
-import { motion } from 'motion/react'
+import { motion } from "motion/react";
 
 const Navbar = () => {
   const links = [
@@ -28,10 +28,11 @@ const Navbar = () => {
 
   return (
     <motion.header
-    initial={{opacity: 0, y: -60}}
-    animate={{opacity: 1, y: 0  }}
-    transition={{duration: 0.8, ease:" easeInOut"}}
-    className="fixed top-0 left-0 right-0 z-50 mx-5 mt-2  text-white">
+      initial={{ opacity: 0, y: -60 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.8, ease: " easeInOut" }}
+      className="fixed top-0 left-0 right-0 z-50 mx-5 mt-2  text-white"
+    >
       <nav className="font-google-sans mx-auto z-20 flex max-w-[800px] items-center justify-between backdrop-blur-2xl rounded-full px-10 py-2 border border-gray-400/10 bg-white/10 ">
         <p className="text-lg text-white">
           <a href="#home">ORMEL</a>
@@ -53,8 +54,18 @@ const Navbar = () => {
         </ul>
 
         <div className="flex gap-5">
-          <FaGithub size={25} className="cursor-pointer hover:text-blue/80 duration-100 transition-colors"/>
-          <FaFacebook size={25}  className="cursor-pointer hover:text-blue/80 duration-300 transition-colors"/>
+          <a href="https://github.com/paetormel">
+            <FaGithub
+              size={25}
+              className="cursor-pointer hover:text-blue/80 duration-100 transition-colors"
+            />
+          </a>
+          <a href="https://www.facebook.com/Ormelpaet08/">
+            <FaFacebook
+              size={25}
+              className="cursor-pointer hover:text-blue/80 duration-300 transition-colors"
+            />
+          </a>
         </div>
       </nav>
     </motion.header>
